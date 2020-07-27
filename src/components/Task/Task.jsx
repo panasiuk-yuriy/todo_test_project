1b41c022589ecbbe877eb72308d552ab931715d7
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-07-27 10:50:59 +0300

    fix bug whith text

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { deleteTask , changeStatus } from '../../store/tasks';
+import { deleteTask, changeStatus } from '../../store/tasks';
 import cn from 'classnames';
 import { Popup } from '../PopUp/PopUpWindow';
 
 import './Task.scss';
 
 export const Task = (props) => {
-  const { name, done, id} = props;
+  const { name, done, id } = props;
 
   const dispatch = useDispatch();
-  const [activeDelete, setActiveDelete]= useState(false);
-  
+  const [activeDelete, setActiveDelete] = useState(false);
+
   const handleDelete = (taskId) => {
     dispatch(deleteTask(taskId))
     setActiveDelete(false)
@@ -19,24 +19,26 @@ export const Task = (props) => {
 
   return (
     <li>
-      <div className="task"
-      role="presentation"
-      onClick={()=> dispatch(changeStatus(id, done))}>
-      <label className={cn("task__name", {'task__done': done})}>
-        {name}
-      </label >
-      {done &&
-        <button className="task__delete-btn btn" type='button' 
-          onClick={() => setActiveDelete(true)}>delete</button>}
+      <div className="task">
+        <p
+          onClick={() => dispatch(changeStatus(id, done))}
+          className={cn("task__name", { 'task__done': done })}
+          role="presentation"
+        >
+          {name}
+        </p >
+        {done &&
+          <button className="task__delete-btn btn" type='button'
+            onClick={() => setActiveDelete(true)}>delete</button>}
       </div>
       {activeDelete &&
-          <Popup
-            name={name}
-            text='Do you realy want delete?'
-            closePopup={() => setActiveDelete(false)}
-            confirmAction={() => handleDelete(id)}
-          />
-        }
+        <Popup
+          name={name}
+          text='Do you realy want delete?'
+          closePopup={() => setActiveDelete(false)}
+          confirmAction={() => handleDelete(id)}
+        />
+      }
     </li>
   )
 }
